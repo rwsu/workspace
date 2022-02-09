f95a17295383ea70ba3f7ca78c7d5e08158fd476
Richard Su <rwsu@redhat.com>
2022-02-08 16:39:16 -0800

    Install assisted-service with AgentClusterInstall

--- add-assisted-service-to-agent-cluster-install/apply-manifests.sh
+++ add-assisted-service-to-agent-cluster-install/apply-manifests.sh
@@ -9,13 +9,21 @@ sed "s#replace-with-public-ssh-key#$PUBLIC_SSH_KEY#" infraenv.yaml.in > infraenv
 # for now this is returning the external ip address of the postgres service in minikube
 BOOSTRAP_POSTGRES_IP_ADDRESS=$(kubectl get services -n assisted-installer | grep postgres |  tr -s ' ' | cut -d ' ' -f 4)
 
+EC_PRIVATE_KEY=$(cat ec-private-key.pem)
+EC_PUBLIC_KEY=$(cat ec-public-key.pem)
+
 sed "s#replace-with-bootstrap-postgres-ip-address#$BOOSTRAP_POSTGRES_IP_ADDRESS#" ./assisted-service-manifests/postgres-import-job.yaml.in > ./assisted-service-manifests/postgres-import-job.yaml
-sed "s#replace-with-bootstrap-postgres-ip-address#$BOOSTRAP_POSTGRES_IP_ADDRESS#" infrastructure-operator-install-manifests.yaml.in > infrastructure-operator-install-manifests.yaml
+sed "s#replace-with-bootstrap-postgres-ip-address#$BOOSTRAP_POSTGRES_IP_ADDRESS#" assisted-service-install-manifests.yaml.in > assisted-service-install-manifests.yaml
+sed "s#replace-with-ec-private-key#$EC_PRIVATE_KEY#" ./assisted-service-manifests/assisted-service-local-auth-key.yaml.in > ./assisted-service-manifests/assisted-service-local-auth-key.yaml
+sed "s#replace-with-ec-public-key#$EC_PUBLIC_KEY#" ./assisted-service-manifests/assisted-service-local-auth-key.yaml.in > ./assisted-service-manifests/assisted-service-local-auth-key.yaml
+sed "s#replace-with-ec-private-key#$EC_PRIVATE_KEY#" assisted-service-install-manifests.yaml.in > assisted-service-install-manifests.yaml
+sed "s#replace-with-ec-public-key#$EC_PUBLIC_KEY#" assisted-service-install-manifests.yaml.in > assisted-service-install-manifests.yaml
+
 
 kubectl apply -f namespace.yaml
 kubectl apply -f pull-secret.yaml
 kubectl apply -f cluster-image-set.yaml
 kubectl apply -f cluster-deployment.yaml
-kubectl apply -f infrastructure-operator-install-manifests.yaml
+kubectl apply -f assisted-service-install-manifests.yaml
 kubectl apply -f agent-cluster-install.yaml
 kubectl apply -f infraenv.yaml
